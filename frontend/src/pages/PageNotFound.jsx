9d7190c447e1ac792a6bf067988a700bbf974ee9
Nishant Desai <nish13199@gmail.com>
2025-10-01 17:08:53 -0500

    Adding frontend setup
    Added basic frontend pages and setup routes for auth with a form.

---
+++ frontend/src/pages/PageNotFound.jsx
@@ -0,0 +1,8 @@
+function PageNotFound() {
+    return <div>
+        <h1>404 Not Found</h1>
+        <p>The page you're looking for doesn't exist (yet...)</p>
+    </div>
+}
+
+export default PageNotFound
